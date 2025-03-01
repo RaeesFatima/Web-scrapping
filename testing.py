927545fcc5191886e726023734ba5480f91ab096
= <raeesyousaf555@gmail.com>
2025-03-01 23:49:51 +0500

    my-min-heap

--- testing.py
+++ testing.py
@@ -1,6 +1,23 @@
-
 def hello(arr):
-    print("Hello, World! from function()")
-    # arr=[1,2,3,4]
-    print(arr)
-hello([4,10,3,5,1])
+    n=len(arr)
+    last=n//2-1
+
+    for i in range (last,-1,-1):
+        left=2*i+1
+        right=2*i+2
+
+        if (arr[left] < arr[i]):
+            temp=arr[i]
+            arr[i]=arr[left]
+            arr[left]=temp
+
+        if (arr[right] < arr[i]):
+            temp=arr[i]
+            arr[i]=arr[right]
+            arr[right]=temp  
+
+    print("here is the final min-heaped array",arr)
+
+    
+arr=[4,10,3,5,1]
+hello(arr)
