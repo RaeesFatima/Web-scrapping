c37075f8d3347b3657e84d790dda3f5a9651eb51
= <raeesyousaf555@gmail.com>
2025-03-07 15:43:41 +0500

    inheritance and encapsulation

--- OOP/start.py
+++ OOP/start.py
@@ -1,18 +1,24 @@
 class Car:
     def __init__(self,brand,model):
-        self.brand=brand
+        self.__brand=brand # __ means private
         self.model=model
 
+    #encapsulation
     def full_name(self):
         return f"{self.brand} {self.model}"
     
+    def get_brand(self):
+        return self.__brand
+
+#inheritance  
 class ElectricCar(Car):
     def __init__(self, brand, model,battery_size):
         super().__init__(brand, model)
         self.battery_size=battery_size
 
 my_tesla=ElectricCar("tesla","model s","85kwh")
-print(my_tesla.brand)
+# print(my_tesla.brand) #if not encapsulated
+print(my_tesla.get_brand()) #if encapsulated
 print(my_tesla.model)
 print(my_tesla.battery_size)
 
