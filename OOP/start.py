01ad9063e5dc278af96eb51c7d7b292aa0306a67
= <raeesyousaf555@gmail.com>
2025-03-07 17:13:20 +0500

    static methods

--- OOP/start.py
+++ OOP/start.py
@@ -1,11 +1,13 @@
 class Car:
+    total_car=0
     def __init__(self,brand,model):
-        self.__brand=brand # __ means private
+        self.__brand=brand      # __ means private
         self.model=model
-
+        Car.total_car+=1
     
     def full_name(self):
         return f"{self.brand} {self.model}"
+    
     #encapsulation
     def get_brand(self):
         return self.__brand
@@ -14,11 +16,18 @@ class Car:
     def fuel_type(self):
         return "petrol or diesel"
 
+    #static method
+    @staticmethod
+    def general_description():
+        return "cars are mean of transport"
+    
+    
 #inheritance  
 class ElectricCar(Car):
     def __init__(self, brand, model,battery_size):
         super().__init__(brand, model)
         self.battery_size=battery_size
+
     #polymorphism
     def fuel_type(self):
         return "electric charge"
@@ -36,3 +45,8 @@ print(my_car.fuel_type())
 # print(my_car.brand)
 # print(my_car.model)
 # print(my_car.full_name())
+
+# print(my_car.general_description())     #cause method is static
+print(Car.total_car)
+
+print(Car.general_description())
