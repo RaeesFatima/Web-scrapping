0cc3e2c4c89f9f32dd9f3f4b65f683b59b86aa73
= <raeesyousaf555@gmail.com>
2025-03-10 15:01:30 +0500

    :/

--- Scrapy/firstscrapy/firstscrapy/spiders/quotes_spider.py
+++ Scrapy/firstscrapy/firstscrapy/spiders/quotes_spider.py
@@ -3,11 +3,13 @@ from ..items import FirstscrapyItem
 class QuotesSpider(scrapy.Spider):
     name='quotes'       #name of spider
     #below are the urls we want to scrap
+    page_number=2
     start_urls=[
-        'http://quotes.toscrape.com/'
+        'https://quotes.toscrape.com/page/1/'
+        # 'http://quotes.toscrape.com/'
     ]
 
-    def parse(self, response):       #response contains the source code which here is title text
+    def parse(self, response):       #response contains the source code
         all_div_quotes=response.css('div.quote')
         items=FirstscrapyItem()
         for quotes in all_div_quotes:
@@ -21,7 +23,12 @@ class QuotesSpider(scrapy.Spider):
 
             yield items
 
-        next_page=response.css('li.next a::attr(href)').get()
+        # next_page=response.css('li.next a::attr(href)').get()
+        # if next_page is not None:
+        #     yield response.follow(next_page,callback=self.parse)
 
-        if next_page is not None:
-            yield response.follow(next_page,callback=self.parse)
+        #pagination here
+        next_page='https://quotes.toscrape.com/page/'+str(QuotesSpider.page_number)+'/'
+        if QuotesSpider.page_number<11:
+            QuotesSpider.page_number+=1
+            yield response.follow(next_page, callback=self.parse)
